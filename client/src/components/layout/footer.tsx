d2553e39aaba6bd264dd5a55e03462ac82896736
Rakesh1002 <rakeshroushan@Live.com>
2025-01-16 14:25:09 +0530

    deployement ready

--- client/src/components/layout/footer.tsx
+++ client/src/components/layout/footer.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { MessageSquare } from "lucide-react";
 import { Container } from "@/components/ui/container";
 import Image from "next/image";
 
